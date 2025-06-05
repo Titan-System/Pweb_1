fa9d4e5b07eba5fe354dad3d63305c8725df4629
rubenzurita90 <141333580+rubenzurita90@users.noreply.github.com>
2025-06-05 12:25:56 -0300

    Update aboutUs Nuevo Modal Tabla de pestañas

--- assets/script/sobreNosotros.js
+++ assets/script/sobreNosotros.js
@@ -14,3 +14,14 @@ li.forEach( (cadaLi , i )=>{
             bloque[i].classList.add('activo')
         })
 })
+
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+
+openBtn.addEventListener('click',()=>{
+    modal.classList.add("open");
+})
+closeBtn.addEventListener('click',()=>{
+    modal.classList.remove("open")
+})
