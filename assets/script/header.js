3c826d84133ace6dd2f09f479e200bd86ecdcb21
Erik Medina <46094137+erme07@users.noreply.github.com>
2025-06-09 17:13:03 -0300

    Update header.js

--- assets/script/header.js
+++ assets/script/header.js
@@ -7,5 +7,16 @@ document.addEventListener('DOMContentLoaded', function () {
         sidebar.classList.toggle('open');
         header.classList.toggle('open');
     });
+    document.addEventListener('click', (e) => {
+        if (e.target.id === 'darkModeToggle') {
+            document.body.classList.toggle('dark-mode');
+            const isDarkMode = document.body.classList.contains('dark-mode');
+            localStorage.setItem('darkMode', isDarkMode);
+        }
+    });
   });
+
+if (localStorage.getItem('darkMode') === 'true') {
+    document.body.classList.add('dark-mode');
+}
   
